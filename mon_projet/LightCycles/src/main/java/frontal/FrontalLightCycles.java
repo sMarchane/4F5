28ad755f0bb637542a777e5b1177e0aef7f4ca29
Marchane <1922145@cmontmorency.qc.ca>
2022-02-17 17:20:48 -0500

    module03

--- mon_projet/LightCycles/src/main/java/frontal/FrontalLightCycles.java
+++ mon_projet/LightCycles/src/main/java/frontal/FrontalLightCycles.java
@@ -1,24 +1,23 @@
 package frontal;
 import static ca.ntro.app.tasks.frontend.FrontendTasks.window;
 
+import ca.ntro.app.NtroApp;
 import ca.ntro.app.frontend.FrontendFx;
 import ca.ntro.app.frontend.ViewRegistrarFx;
 import ca.ntro.app.frontend.events.EventRegistrar;
 import ca.ntro.app.services.Window;
 import ca.ntro.app.tasks.frontend.FrontendTasks;
+import frontal.taches.Initialisation;
+import frontal.vues.VueInterieur;
+import frontal.vues.VueRacine;
 
 public class FrontalLightCycles implements FrontendFx{
 
 
 	@Override
 	public void createTasks(FrontendTasks tasks) {
-		tasks.task("afficherFenetre")
-		.waitsFor(window())
-		.thenExecutes(inputs ->{
-			Window window = inputs.get(window());
-			window.show();
-		});
-		
+
+		Initialisation.creerTaches(tasks);
 	}
 
 	@Override
@@ -37,6 +36,14 @@ public class FrontalLightCycles implements FrontendFx{
 	public void registerViews(ViewRegistrarFx registrar) {
 		
 		
+		registrar.registerView(VueRacine.class, "/racine.xml");
+		
+		registrar.registerView(VueInterieur.class, "/Vue_interieur.xml");
+		
+		registrar.registerStylesheet("/LightCycles.css");
+		
+		registrar.registerDefaultResources("/chaines_fr.properties");
+		registrar.registerResources(NtroApp.locale("en"), "/chaines_en.properties");
 	}
 
 }
---
+++ mon_projet/LightCycles/src/main/java/frontal/taches/Initialisation.java
@@ -0,0 +1,108 @@
+package frontal.taches;
+
+import static ca.ntro.app.tasks.frontend.FrontendTasks.create;
+import static ca.ntro.app.tasks.frontend.FrontendTasks.created;
+import static ca.ntro.app.tasks.frontend.FrontendTasks.viewLoader;
+import static ca.ntro.app.tasks.frontend.FrontendTasks.window;
+
+import ca.ntro.app.frontend.ViewLoader;
+import ca.ntro.app.services.Window;
+import ca.ntro.app.tasks.frontend.FrontendTasks;
+import frontal.vues.VueInterieur;
+import frontal.vues.VueRacine;
+import frontal.vues.VueInterieur;
+import frontal.vues.VueInterieur;
+
+
+public class Initialisation {
+	
+	private static void installerVueRacine(FrontendTasks tasks) {
+		tasks.task("installerVueRacine")
+		
+			.waitsFor(window())
+			
+			.waitsFor(created(VueRacine.class))
+			
+			.thenExecutes(inputs -> {
+				
+				VueRacine vueRacine = inputs.get(created(VueRacine.class));
+				Window window = inputs.get(window());
+				
+				window.installRootView(vueRacine);
+			});
+	}
+	
+	public static void creerVueRacine(FrontendTasks tasks) {
+		tasks.task(create(VueRacine.class))
+		
+			.waitsFor(viewLoader(VueRacine.class))
+			
+			.thenExecutesAndReturnsValue(inputs -> {
+				
+				ViewLoader<VueRacine> viewLoader = inputs.get(viewLoader(VueRacine.class));
+				
+				VueRacine vueRacine = viewLoader.createView();
+				
+				return vueRacine;
+			});
+	}
+	public static void creerTaches(FrontendTasks tasks) {
+		afficherFenetre(tasks);
+		
+		creerVueRacine(tasks);
+		creerVueInterieur(tasks);
+		
+		
+		installerVueRacine(tasks);
+		installerVueInterieur(tasks);
+		
+		
+	}	
+	
+	private static void afficherFenetre(FrontendTasks tasks) {
+		
+		tasks.task("afficherFenetre")
+		
+			.waitsFor(window())
+			
+			.thenExecutes(inputs -> {
+				
+				Window window = inputs.get(window());
+				
+				window.show();
+			});	
+		
+	}
+	private static void installerVueInterieur(FrontendTasks tasks) {
+		tasks.task("installerVueInterieur")
+		
+			.waitsFor(created(VueRacine.class))
+			
+			
+			.waitsFor(created(VueInterieur.class))
+			
+			.thenExecutes(inputs ->{
+				
+				VueRacine vueRacine = inputs.get(created(VueRacine.class));
+				VueInterieur vueInterieur = inputs.get(created(VueInterieur.class));
+				
+				vueRacine.afficherSousVue(vueInterieur);
+			});
+	}
+	private static void creerVueInterieur(FrontendTasks tasks) {
+		tasks.task(create(VueInterieur.class))
+		
+			.waitsFor(viewLoader(VueInterieur.class))
+			
+			.thenExecutesAndReturnsValue(inputs -> {
+				
+				ViewLoader<VueInterieur> viewLoader = inputs.get(viewLoader(VueInterieur.class));
+				
+				VueInterieur vueInterieur = viewLoader.createView();
+				
+				return vueInterieur;
+			});
+	}
+	
+	
+}
